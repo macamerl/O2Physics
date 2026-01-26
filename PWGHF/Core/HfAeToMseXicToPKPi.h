8b0ddf29383c3a2e4d30b889feee7f42508e4191
INFNuser <INFNuser@mtcamerl.ba.infn.it>
2026-01-26 11:13:23 +0100

    AE and MSE for Xic in pKpi

---
+++ PWGHF/Core/HfAeToMseXicToPKPi.h
@@ -0,0 +1,162 @@
+// Copyright 2020-2022 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file HfAeToMseXicToPKPi.h
+/// \brief Class to compute the mse for the Autoencoder for Xic+ → p K- π+ analysis selections
+/// \author Maria Teresa Camerlingo
+
+#ifndef PWGHF_CORE_HFAETOMSEXICTOPKPI_H_
+#define PWGHF_CORE_HFAETOMSEXICTOPKPI_H_
+
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "rapidjson/document.h"
+#include "rapidjson/filereadstream.h"
+
+
+#include "PWGHF/Core/HfMlResponse.h"
+namespace o2::analysis
+{
+	template <typename TypeOutputScore = float>
+	class HfAeToMseXicToPKPi : public HfMlResponse<TypeOutputScore>
+	{
+	  public:
+		/// Default constructor
+  		HfAeToMseXicToPKPi() = default;
+  		/// Default destructor
+  		virtual ~HfAeToMseXicToPKPi() = default;
+  		
+  		std::vector<float> yScaled, yOutRescaled;
+  		std::vector<std::vector<float>> MinMatrix, MaxMatrix;
+  		
+  		// Fill the scaleMin and scaleMax per each Ptbins
+  		void scalePars_matrices(std::vector<std::string> preprocessFiles, std::vector<std::vector<float>>& MinMatrix, std::vector<std::vector<float>>& MaxMatrix){
+  			MinMatrix.clear();
+    			MaxMatrix.clear();
+  		        MinMatrix.resize(preprocessFiles.size());
+    			MaxMatrix.resize(preprocessFiles.size());
+  			for(size_t i =0; i < preprocessFiles.size(); i++){
+  			// Open the file for reading
+    				FILE* fp = fopen(preprocessFiles.at(i).c_str(), "r");
+    				char readBuffer[65536];
+    				rapidjson::FileReadStream is(fp, readBuffer,
+                                 sizeof(readBuffer));
+    			// Parse the JSON data 
+    			// using a Document object
+    				rapidjson::Document d;
+   				d.ParseStream(is);
+    			// Close the file
+    				fclose(fp);
+    				if (d.HasParseError()) {
+	    				LOG(fatal) << "Parsing FAILED!\n";
+	    				LOG(fatal) << "Error at offset: " << d.GetErrorOffset() << "\n";
+				} else {
+	    				LOG(info) << "JSON document successfully parsed for ML pre-processing.\n";
+	    				for(size_t j = 0; j < d["scaleMin"].Size(); j++){
+	    					(MinMatrix.at(i)).push_back(d["scaleMin"][j].GetFloat());
+	    					(MaxMatrix.at(i)).push_back(d["scaleMax"][j].GetFloat());
+	                                }
+				}	
+  			}
+  		}
+		
+		std::vector<float> getScalePars(int pTBin, std::vector<std::vector<float>> scaleParsMatrix){
+			if( pTBin >= int(scaleParsMatrix.size()) || pTBin < 0) {
+				LOG(fatal) << "No JSON for ML pre-processing in PtBins: " << pTBin;
+				return {}; 
+				}
+			else {
+				//LOG(info) << "PtBins: " << pTBin << "size "<< scaleParsMatrix.at(pTBin).size();
+				return scaleParsMatrix.at(pTBin);
+				}
+		}
+		
+		void setMinMaxScaling(std::vector<float>& yOut, std::vector<float> yIn, std::vector<float> scaleMin, std::vector<float> scaleMax)
+		{	yOut.clear();//initial clear to avoid multiple filling if setMinMax o setScaling are called more than once			
+			for (size_t j = 0; j < yIn.size(); ++j)
+			{ //for over the features 
+					//MinMax scaling of the input features
+					LOG(debug)<<"--------------> MinMax scaling Debug \t"<<scaleMin.at(j)<<"\t"<<scaleMax.at(j);
+					yOut.push_back((yIn.at(j) - scaleMin.at(j))/(scaleMax.at(j)- scaleMin.at(j)));
+					LOG(debug)<<"Feature = "<<j<<" ----> input = "<<yIn.at(j)<<" scaled feature = "<< yOut.at(j); 					
+			}
+		}	  		
+  		//----  External preprocessing scaling
+  		void setScaling(bool scaleFlag, int scaleType, /*input features of a candidate*/ std::vector<float> yIn, std::vector<float> scaleMin, std::vector<float> scaleMax){ //it takes the bool flag and scaling parameters configurables in taskXic
+  			yScaled.clear();
+  		  	if( scaleFlag == false){ LOG(debug)<<"No external preprocessing transformation will be applied"; 
+  		  		yScaled.assign(yIn.begin(), yIn.end());
+  		  	} else{
+				  		if(scaleType == 1){
+				  			LOG(debug)<<"MinMax scaling will be applied"; 
+				  			setMinMaxScaling(yScaled, yIn, scaleMin, scaleMax);
+				  		}//... with scaleType > 1 we could add other preprocessing trasformations	  	
+  		  	}
+  		}
+  		std::vector<float> getPreprocessedFeatures(){ 
+  		  for (size_t j = 0; j < yScaled.size(); ++j) LOG(debug)<<"Global scaled feature = "<< yScaled.at(j); 
+  			return  yScaled; 			
+  		}
+  		//Reverse preprocessing - output postprocessing
+  		void unsetMinMaxScaling(std::vector<float>& yOut, std::vector<float> yIn, std::vector<float> scaleMin, std::vector<float> scaleMax)
+		{	yOut.clear();//initial clear to avoid multiple filling if setMinMax o setScaling are called more than once				
+			for (size_t j = 0; j < yIn.size(); ++j)
+			{ //for over the features 
+					//MinMax scaling of the input features
+					LOG(debug)<<"--------------> MinMax unscaling Debug \t"<<scaleMin.at(j)<<"\t"<<scaleMax.at(j);
+					yOut.push_back(yIn.at(j)*(scaleMax.at(j)- scaleMin.at(j))+ scaleMin.at(j));
+					LOG(debug)<<"Unscaling output = "<<j<<" ----> input = "<<yIn.at(j)<<" rescaled output = "<< yOut.at(j); 					
+			}
+		}	  		
+
+		void unsetScaling(bool scaleFlag, int scaleType, /*AE output*/ std::vector<float> yIn, std::vector<float> scaleMin, std::vector<float> scaleMax){ //it takes the bool flag and scaling parameters configurables in taskXic
+  			yOutRescaled.clear();
+  		  	if( scaleFlag == false){ LOG(debug)<<"No external preprocessing transformation will be applied"; 
+  		  		yOutRescaled.assign(yIn.begin(), yIn.end());
+  		  	} else{
+				  		if(scaleType == 1){
+				  			LOG(debug)<<"MinMax unscaling will be applied"; 
+				  			unsetMinMaxScaling(yOutRescaled, yIn, scaleMin, scaleMax);
+				  		}//... with scaleType > 1 we could add other preprocessing trasformations	  	
+  		  	}
+  		}
+  		std::vector<float> getPostprocessedOutput(){ 
+  		  for (size_t j = 0; j < yOutRescaled.size(); ++j) LOG(debug)<<"Global rescaled AE output = "<< yOutRescaled.at(j); 
+  			return  yOutRescaled; 			
+  		}		
+		//---- MSE function
+		float getMse(std::vector<float>& yTrue, std::vector<float>& yPred){
+				  LOG(debug)<<"Inside getMse sizes "<<yTrue.size()<<"\t"<<yPred.size();
+				  float mse= 0.0f;
+				  float sum = 0.0f;    
+				  for (size_t j = 0; j < yTrue.size(); ++j) LOG(debug)<<"Local Feature = "<<j<<" ----> input = "<<yTrue.at(j)<<" scaled feature = "<< yPred.at(j);
+				  std::vector<float> yTrueScaled = getPreprocessedFeatures();
+				  if( yTrue.size() != yPred.size()){  
+				  	LOG(debug)<< "size of input vector ="<<yTrue.size();
+				  	LOG(debug)<< "size of AE output vector ="<< yPred.size();
+				  	LOG(fatal) << "vectors of input and predictions don't have the same size";    	
+				  }
+				  else{//MSE    
+						for (size_t j = 0; j < yPred.size(); ++j) { //for over the features 
+								sum += pow(((yTrueScaled).at(j) - (yPred).at(j)), 2); //has dimensions
+								LOG(debug)<<"getMse Local feature = "<<j<<" ----> input = "<<yTrueScaled.at(j)<<" AE prediction = "<< yPred.at(j);           
+						}
+						mse = sum/yPred.size(); //MSE of a candidate 
+						LOG(debug)<<"Local mse "<<mse;
+				  }
+				  return mse;
+			}
+	}; //end of the class 
+	
+
+} // namespace o2::analysis
+#endif // PWGHF_CORE_HFAETOMSEXICTOPKPI_H_
